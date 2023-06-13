da812c9120fcd7fe242b6fc6824b66c25cae2d9b
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-13 16:20:49 +0530

    layout added, hero, navigation, footer, custom button and constants added

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import Image from 'next/image'
 import { CustomButtonProps } from '@/types'
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
   return (
     <button
         disabled={false}
-        type={'button'}
+        type={btnType || 'button'}
         className={`custom-btn ${containerStyles}`}
         onClick={handleClick}
     >
