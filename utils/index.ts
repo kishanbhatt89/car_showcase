1044cef7a1addbd13038475bbf270410efb1f755
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-14 14:12:32 +0530

    Car Details Componenet added with modal

--- utils/index.ts
+++ utils/index.ts
@@ -5,7 +5,7 @@ export async function fetchCars() {
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers})
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {headers: headers})
 
     const result = await response.json()
 
