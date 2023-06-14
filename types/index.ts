03a8776a55ef8ae672c251685eb5fe3329eec08d
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-14 16:09:01 +0530

    Custom filter functionality added

--- types/index.ts
+++ types/index.ts
@@ -10,8 +10,14 @@ export interface CustomButtonProps {
     isDisabled?: boolean
 }
 
+export interface OptionProps {
+    title: string
+    value: string
+}
+
 export interface CustomFilterProps {
     title: string
+    options: OptionProps[]
 }
 
 export interface SearchManufacturerProps {
